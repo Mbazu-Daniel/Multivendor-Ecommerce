1c4fc6ed9ce537ac32a7512dc1c2f1df652a27ff
Mbazu Daniel <Mbazudaniel97@gmail.com>
2023-03-09 16:37:24 +0100

    update: middlewares to control errors

--- controller/users.controllers.js
+++ controller/users.controllers.js
@@ -1,19 +1,21 @@
 const User = require("../models/users.models")
-
-const createUser = async (req, res) => {
-    const email = req.body.email
-    console.log("what am getting", email)
-    const findUser = await User.findOne({email: email})
-    if(!findUser) {
-        // create a new user
-const newUser = User.create(req.body)
-res.status(201).json(newUser)
-    } else {
-        // user already exist
-        res.json({msg: "User Already Exist",success: false})
+const asyncHandler = require("express-async-handler")
+const createUser = asyncHandler(
+    async (req, res) => {
+        const email = req.body.email
+        console.log("what am getting", email)
+        const findUser = await User.findOne({email: email})
+        if(!findUser) {
+            // create a new user
+    const newUser = await User.create(req.body)
+    res.status(201).json(newUser)
+        } else {
+            // user already exist
+            throw new Error("User Already Exist")
+        }
     }
-}
-
+    
+)
 module.exports = {
     createUser
 };
--- server.js
+++ server.js
@@ -5,15 +5,23 @@ const MONGO_URI = process.env.MONGO_URI
 require("dotenv").config()
 const PORT = process.env.PORT || 5000
 const connectDB = require("./config/connectDB")
+const { notFound, errorHandler } = require("./middleware/errorHandler")
 const authRouter = require("./routes/users.routes")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
 // ROUTERS
 app.use("/api/v1/auth",authRouter )
 
+// MIDDLEWARE
+app.use(notFound)
+app.use(errorHandler)
+
+
+// HomePage
 app.use("/", (req, res) => {
-    res.send("Hello From the other side")
+    res.send("Welcome to Multi-Vendor E-commerce ")
 } )
  
 const start = async () => {
