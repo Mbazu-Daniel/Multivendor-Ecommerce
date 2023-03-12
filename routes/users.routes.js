8d4ff7046710d7dbc39af980576c43a6b3f167ee
Mbazu Daniel <Mbazudaniel97@gmail.com>
2023-03-12 22:56:26 +0100

    Auth middleware route update for get single users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -8,11 +8,13 @@ const {
   deleteUser,
   updateUser,
 } = require("../controller/users.controllers");
+const { authMiddleware } = require("../middleware/authMiddleware");
+
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/users", getAllUsers);
-router.get("/user/:id", getAUser);
+router.get("/user/:id", authMiddleware, getAUser);
 router.delete("/user/:id", deleteUser);
 router.patch("/user/:id", updateUser);
 
